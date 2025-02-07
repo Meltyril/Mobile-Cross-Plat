792d8b2c1fd4ef68a50cea34f5efe0eb671aae52
Meltyril <111682264+Meltyril@users.noreply.github.com>
2025-02-07 11:17:24 +0700

    Week01

---
+++ LabWeek01/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <image></image>
+      <Text>Darryl Kurniawan - 00000078767</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
