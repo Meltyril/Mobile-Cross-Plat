7f853252a96b024fc229fed84012a81ef94bd22d
Meltyril <111682264+Meltyril@users.noreply.github.com>
2025-03-14 09:44:04 +0700

    Update App.js

--- LabWeek01/App.js
+++ LabWeek01/App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <image></image>
       <Text>Darryl Kurniawan - 00000078767</Text>
       <StatusBar style="auto" />
     </View>
